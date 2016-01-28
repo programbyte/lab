18569e692ec0fbc9b71d1cad8ab02df08a9de5a5
iomltn <iomltn@gmail.com>
2016-01-28 17:17:02 -0300

    Modificacao Janela.java

--- Janela.java
+++ Janela.java
@@ -1,10 +1,12 @@
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Janela extends JFrame {
 	public Janela() {
 		super("Exemplo de janela");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(600, 400);
+		add(new JLabel("Teste de swing"));
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
